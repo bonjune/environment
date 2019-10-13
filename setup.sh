c4d37ba298bee357ce14359b1d7bf8cb2cb6aeac
Kihong Heo <kheo@cis.upenn.edu>
2019-10-13 17:12:31 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 ln -s $(pwd)/.bash_profile ~/.bash_profile
 ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.vim ~/.vim
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.screenrc ~/.screenrc
-
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
