0d7e5e0e4a5f386d029de7385a9b0cab6cb19576
Kihong Heo <kheo@cis.upenn.edu>
2019-10-14 03:27:14 -0400

    update

--- .zshrc
+++ .zshrc
@@ -65,7 +65,7 @@ ZSH_THEME="agnoster"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git osx docker zsh-syntax-highlighting)
+plugins=(git osx docker zsh-syntax-highlighting screen)
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ set -e
 # install oh-my-zsh
 if [[ ! -d $HOME/.oh-my-zsh ]]; then
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
