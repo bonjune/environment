a10d968f5a1257bd239df43f36f2aecd22f72158
Kihong Heo <kihong.heo@gmail.com>
2020-03-29 18:17:36 +0900

    add zsh auto suggestion setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ fi
 if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
 ln -s $(pwd)/.bash_profile ~/.bash_profile
 ln -s $(pwd)/.bashrc ~/.bashrc
