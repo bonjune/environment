26e4b9f6756192232d8522ce30607b9ee159ce63
Bongjun <bongjun.jang@kaist.ac.kr>
2024-01-27 03:43:50 +0000

    [Script] update

--- setup.sh
+++ setup.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-# install oh-my-zsh
-if [[ ! -d $HOME/.oh-my-zsh ]]; then
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
-if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-
 ln -s -f $(pwd)/.bash_profile ~/.bash_profile
 ln -s -f $(pwd)/.bashrc ~/.bashrc
 ln -s -f $(pwd)/.zshrc ~/.zshrc
@@ -34,3 +23,17 @@ ln -s -f $(pwd)/.pylintrc ~/.pylintrc
 
 # python support for neovim
 pip3 install pynvim
+
+curl https://pyenv.run | bash
+
+# install oh-my-zsh
+if [[ ! -d $HOME/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
